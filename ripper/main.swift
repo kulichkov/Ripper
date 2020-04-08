d48be39178261493bd0d38a98a1eac0f13cd3bf1
kulichkov <mikhail.kulichkov@gmail.com>
2020-04-08 15:06:44 +0300

    Video/audio downloading/merging works

--- ripper/main.swift
+++ ripper/main.swift
@@ -8,6 +8,8 @@
 
 import Foundation
 
+typealias Path = String
+
 let args = CommandLine.arguments
 let console = ConsoleIO()
 
@@ -43,7 +45,7 @@ if args.count == 3 {
 	getJSONDict(url: masterURL) { result in
 		switch result {
 		case .success(let master):
-			console.writeMessage("Master file is downloaded...")
+			console.writeMessage("Master file downloaded")
 
 			let videoOutputPath = currentPath + "/\(master.clip_id).m4v"
 			let segmentsFolder = currentPath + "/\(master.clip_id)"
@@ -62,18 +64,49 @@ if args.count == 3 {
 			let videoBase64String = video.init_segment
 			let audioBase64String = audio.init_segment
 
-			//FileManager.default.createFile(atPath: videoOutputPath, contents: Data(base64Encoded: videoBase64String))
-
 			//			let videoFile = try? FileHandle(forWritingTo: URL(fileURLWithPath: videoOutputPath))
 			//			videoFile?.seekToEndOfFile()
 			//			defer { videoFile?.closeFile() }
-			
+
+			// Video processing
 			console.writeMessage("Starting download video...")
-			downloadMedia(video, to: videoSegmentsFolder, baseURL: mediaBaseURL)
+			let videoPaths = downloadMedia(video, to: videoSegmentsFolder, baseURL: mediaBaseURL)
 
+			console.writeMessage("Video downloaded. Starting merging video segments...")
+
+			mergeMediaFiles(
+				videoPaths,
+				to: videoOutputPath,
+				withInitData: Data(base64Encoded: videoBase64String))
+			console.writeMessage("Video merged.")
+
+			console.writeMessage("Deleting video segment folder...")
+			do {
+				try FileManager.default.removeItem(atPath: videoSegmentsFolder)
+				console.writeMessage("Video segment folder deleted")
+			} catch {
+				console.writeMessage(error.localizedDescription, to: .error)
+			}
+
+			// Audio processing
 			console.writeMessage("Starting download audio...")
-			downloadMedia(audio, to: audioSegmentsFolder, baseURL: mediaBaseURL)
+			let audioPaths = downloadMedia(audio, to: audioSegmentsFolder, baseURL: mediaBaseURL)
 
+			console.writeMessage("Audio downloaded. Starting merging audio segments...")
+
+			mergeMediaFiles(
+				audioPaths,
+				to: audioOutputPath,
+				withInitData: Data(base64Encoded: audioBase64String))
+			console.writeMessage("audio merged.")
+
+			console.writeMessage("Deleting audio segment folder...")
+			do {
+				try FileManager.default.removeItem(atPath: audioSegmentsFolder)
+				console.writeMessage("Audio segment folder deleted")
+			} catch {
+				console.writeMessage(error.localizedDescription, to: .error)
+			}
 
 		case .failure(let error):
 			print(error)
@@ -83,22 +116,46 @@ if args.count == 3 {
 	printUsage()
 }
 
+func mergeMediaFiles(_ paths: [Path], to output: Path, withInitData initData: Data?) {
+	guard FileManager.default.createFile(atPath: output, contents: initData) else {
+		console.writeMessage("Error creating file \(output)", to: .error)
+		return
+	}
 
-func downloadMedia(_ media: Media, to localFolder: String, baseURL: URL) {
+	let output = FileHandle(forWritingAtPath: output)
+	output?.seekToEndOfFile()
+
+	defer { output?.closeFile() }
+
+	do {
+		for path in paths {
+			output?.write(try Data(contentsOf: URL(fileURLWithPath: path)))
+			try FileManager.default.removeItem(atPath: path)
+		}
+	} catch {
+		console.writeMessage(error.localizedDescription, to: .error)
+		return
+	}
+}
+
+func downloadMedia(_ media: Media, to localFolder: Path, baseURL: URL) -> [Path] {
 	do {
 		try FileManager.default.createDirectory(
 			atPath: localFolder,
 			withIntermediateDirectories: true)
 	} catch {
 		console.writeMessage(error.localizedDescription, to: .error)
-		return
+		return []
 	}
 
 	let mediaURL = baseURL.resolved(to: media.base_url)
 
+	var fileURLs = [Path]()
+
 	let operations = media.segments.map { (segment) -> MediaDownloader in
 		let segmentSourceURL = mediaURL.resolved(to: segment.url)
 		let segmentPath = localFolder + "/" + segment.url
+		fileURLs.append(segmentPath)
 		return MediaDownloader(url: segmentSourceURL, path: segmentPath) { isDownloaded in
 			if isDownloaded {
 				console.writeMessage("data written to \(segmentPath)")
@@ -108,6 +165,7 @@ func downloadMedia(_ media: Media, to localFolder: String, baseURL: URL) {
 
 	let queue = OperationQueue()
 	queue.addOperations(operations, waitUntilFinished: true)
+	return fileURLs
 }
 
 func printUsage() {
